f1e9d92feb850b0f2e4ca4c77f2bfe9648188fe7
Andrey <anohov2012@yandex.ru>
2026-02-05 00:55:24 +0300

    #dz 2 добавил слушатели по клику на кнопки

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
-        setContentView(R.layout.activity_main_1)
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(R.layout.activity_main)
+        val searchButton = findViewById<Button>(R.id.search_btn)
+        searchButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку \"Поиск\"", Toast.LENGTH_SHORT).show()
         }
-         */
+        val mediaButton = findViewById<Button>(R.id.media_btn)
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку \"Медиатека\"", Toast.LENGTH_SHORT).show()
+        }
+        val settingButton = findViewById<Button>(R.id.settings_btn)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener { override fun onClick(v: View?) {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку \"Настройки\"", Toast.LENGTH_SHORT)
+                .show()
+        } }
+        settingButton.setOnClickListener(imageClickListener)
+
     }
 }
