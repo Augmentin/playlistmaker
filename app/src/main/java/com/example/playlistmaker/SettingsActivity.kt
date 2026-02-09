eea6a28b0ff228fd9b6d87dfd9c39ffa72c0db44
Andrey <anohov2012@yandex.ru>
2026-02-09 23:13:10 +0300

    Исправления по ревью

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,19 +1,17 @@
 package com.example.playlistmaker
-import android.content.Intent
 import android.os.Bundle
-
-import android.widget.ImageView
-
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_settings)
-        val back = findViewById<ImageView>(R.id.back_arrow)
-        back.setOnClickListener {
+        val back = findViewById<MaterialToolbar>(R.id.back_toolbar)
+        back.setNavigationOnClickListener {
             finish()
         }
     }
