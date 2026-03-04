790b6f49223f68dac87014262dd697787973feda
Andrey <anohov2012@yandex.ru>
2026-03-04 23:48:48 +0300

    Создание списка песен. Спринт 10

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -13,6 +13,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.search.SongListAdapter
+import com.example.playlistmaker.search.data.Track
 import com.google.android.material.appbar.MaterialToolbar
 
 
@@ -29,7 +33,7 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        val currentView = findViewById<View>(R.id.search_view );
+        val currentView = findViewById<View>(R.id.search_view);
         ViewCompat.setOnApplyWindowInsetsListener(currentView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -45,35 +49,26 @@ class SearchActivity : AppCompatActivity() {
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         inputEditText.setText(current_search)
         clearButton.setOnClickListener {
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(currentView.windowToken, 0)
             inputEditText.setText("")
             inputEditText.clearFocus()
         }
 
-        val simpleTextWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
-            }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!s.isNullOrEmpty()){
-                    inputEditText.hint = ""
-                    current_search = s.toString()
-                }else{
-                    current_search = STRING_DEF
-                }
-                clearButton.isVisible = !s.isNullOrEmpty()
 
+        inputEditText.doOnTextChanged { s: CharSequence?, start: Int, before: Int, count: Int ->
+            if (!s.isNullOrEmpty()) {
+                inputEditText.hint = ""
+                current_search = s.toString()
+            } else {
+                current_search = STRING_DEF
             }
-
-            override fun afterTextChanged(s: Editable?) {
-                // empty
-            }
+            clearButton.isVisible = !s.isNullOrEmpty()
         }
-
-
-        inputEditText.addTextChangedListener(simpleTextWatcher)
+        val songItems = findViewById<RecyclerView>(R.id.songItems)
+        songItems.adapter = SongListAdapter(getTrackList())
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -87,5 +82,46 @@ class SearchActivity : AppCompatActivity() {
         current_search = savedInstanceState.getString(SEARCH_STRING, STRING_DEF)
     }
 
+    private fun getTrackList(): ArrayList<Track> {
+        return arrayListOf(
+            Track(
+                "Smells Like Teen Spirit",
+                "Nirvana",
+                "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "TestSmells Like Teen Spirit Smells Like Teen Spirit Smells Like Teen Spirit Smells Like Teen Spirit",
+                "TESTNirvana Nirvana Nirvana Nirvana Nirvana Nirvana",
+                "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+
+            Track(
+                "Billie Jean",
+                "Michael Jackson",
+                "4:35",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Stayin' Alive",
+                "Bee Gees",
+                "4:10",
+                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Whole Lotta Love",
+                "Led Zeppelin",
+                "5:33",
+                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Sweet Child O'Mine",
+                "Guns N' Roses",
+                "5:03",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+            ),
+        )
+    }
 
 }
